0360d49fa6711e97b0f5e236d3da5258a3309a23
jsyeh <jsyeh@mail.mcu.edu.tw>
2022-04-14 19:55:55 +0800

    modify in 2022

--- countDownJS/countDownJS.js
+++ countDownJS/countDownJS.js
@@ -8,8 +8,8 @@ function windowResized() {
   textSize(width/10);
 }
 function draw() {
-  let destH=20;
-  let destM=0;
+  let destH=14;
+  let destM=50;
   let destS=0;
   background(58,114,191);
   let now = hour()*60*60+minute()*60+second();
@@ -25,8 +25,8 @@ function draw() {
   let line1 = "剩下"+hh+":"+mm+":"+ss;
   textAlign(CENTER,CENTER);
   textSize(width/20);
-  text("文化207線上"+nf(destH,2)+":"+nf(destM,2)+"開始上課", width/2,height/2-width/20);
-  //text("程式設計"+nf(destH,2)+":"+nf(destM,2)+"開始上課", width/2,height/2-width/20);
+  //text("文化207線上"+nf(destH,2)+":"+nf(destM,2)+"開始上課", width/2,height/2-width/20);
+  text("程式設計"+nf(destH,2)+":"+nf(destM,2)+"開始上課", width/2,height/2-width/20);
   //text("距離"+nf(destH,2)+":"+nf(destM,2)+"敏真組meeting開始", width/2,height/2-100);
   //text("距離"+nf(destH,2)+":"+nf(destM,2)+":"+nf(destS,2)+"敏真組meeting開始", width/2,height/2-100);
   textSize(width/10);
